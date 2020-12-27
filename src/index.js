a4edd89ea2776cdb80c3a8f46923bf22b577ee9d
Shoaib Arif <75606050+shoaibarif668@users.noreply.github.com>
2020-12-27 18:34:47 +0500

    updated 12/27/2020

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
-import React from 'react';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './Sass/main.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+
+
+//Inline and internal styling in react is done through making objects #16
+
+import {React} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './Sass/main.css';
+
+let name="Shoaib";
+let image="https://picsum.photos/200/300";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <>
+    <div className="heading">
+        <h1>My name is {name}</h1>
+    </div>
+    <div className="head-img">
+        <img src={image} alt=""/>
+        <img src={image} alt=""/>
+        <img src={image} alt=""/>
+    </div>  
+  </>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
